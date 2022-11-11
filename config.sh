45cd2da877f8131c9d166381721fa4f95f717de3
SirRGB <sirrgb@proton.me>
2022-11-11 20:13:06 +0100

    bringup cedric

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SirRGB"
+export GITHUB_EMAIL="sirrgb@proton.me"
 
-export device=""
+export device="cedric"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Project Dessert"
+export ROM_DIR="${WORKSPACE}/../Des12"
+export ROM_VERSION="12L"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/cedric/A12Dessert.xml"
+export manifest_url="https://github.com/Project-Dessert/android"
+export rom_vendor_name="lineage"
+export branch="snowcone"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
+export ccache="false"
 export ccache_size=""
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="Project-Dessert/releases"
 
 export timezone="UTC"
